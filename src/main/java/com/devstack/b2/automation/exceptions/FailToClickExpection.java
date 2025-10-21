d7eee724a4e802705729aade9ca016ab1c75cfc5
dilmi1108 <dilmihirunika2001@gmail.com>
2025-10-21 09:15:47 +0530

    FailToClickExpection.java added

---
+++ src/main/java/com/devstack/b2/automation/exceptions/FailToClickExpection.java
@@ -0,0 +1,7 @@
+package com.devstack.b2.automation.exceptions;
+
+public class FailToClickExpection extends AutomationExpection{
+    public FailToClickExpection(String message) {
+        super(message);
+    }
+}
