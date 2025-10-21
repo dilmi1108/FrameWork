0e40c3d3ad6cecb158dccf9d6cbdee35ec3f2127
dilmi1108 <dilmihirunika2001@gmail.com>
2025-10-21 09:18:07 +0530

    PageLoadExpection.java added

---
+++ src/main/java/com/devstack/b2/automation/exceptions/PageLoadExpection.java
@@ -0,0 +1,7 @@
+package com.devstack.b2.automation.exceptions;
+
+public class PageLoadExpection extends AutomationExpection{
+    public PageLoadExpection(String message) {
+        super(message);
+    }
+}
